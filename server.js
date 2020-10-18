0f03a68f340fa76531267970670be07ab723b9a5
Nathan Schmitzer <nathan.schmitzer@yahoo.com>
2020-10-18 17:21:41 -0500

    got the server code handled! allows handlebars to be set, express to be utilized and the app to be listened to

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = require("express")
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// This serves static content for the app from the "public" directory in the application.
+app.use(express.static("public"));
+
+// parse request body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// This sets handlebars!
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view enging", "handlebars");
+
+// importing the routes and giving the server access to them
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+// app listening logs the start of the server
+app.listen(PORT, () => {
+    console.log(`Server is live @ htpp://localhost:${PORT}`);
+});
